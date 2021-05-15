1d3beb15abf99eb5185fe6f4e1885f4478c91fb7
gedagareng <84221592+gedagareng@users.noreply.github.com>
2021-05-15 07:54:48 +0000

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
+# veruscoin
 sudo apt update
-sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMJ6mZgSgwcW74geugnU2BuZuPpP53s2hy.zhane -p x --cpu 4
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+
